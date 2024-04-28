db50ea3dc8164944971268117f3e76f15cff2a71
afetism <aismayilova1215@gmail.com>
2024-04-29 02:57:39 +0400

    Add project files.

---
+++ EyeGalery/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Windows;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using ListBox = System.Windows.Controls.ListBox;
+using Image = System.Windows.Controls.Image;
+using EyeGalery.View;
+namespace EyeGalery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+      
+        public ObservableCollection<String> Items { get; set; } = new ObservableCollection<String>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext=this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private void Click_New(object sender, RoutedEventArgs e)
+        {
+            Items.Clear();
+
+            Items=new();
+        }
+        public string root;
+
+        private void Click_Open(object sender, RoutedEventArgs e)
+        {
+
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            dialog.Description = "Select a folder";
+            dialog.UseDescriptionForTitle = true;
+
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                string selectedFolder = dialog.SelectedPath;
+
+                root=selectedFolder;
+                var files = Directory.GetFiles(selectedFolder);
+
+                foreach (string file in files)
+                {
+                    Items.Add(file);
+                }
+            }
+        }
+
+
+
+
+
+
+    
+    
+
+        private void Click_SaveAs(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Exit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Click_Save(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                string selectedFolderPath = dialog.SelectedPath;
+
+                
+                string fileName = "yeniDosya.txt";
+                string filePath = Path.Combine(selectedFolderPath, fileName);
+
+               
+                File.WriteAllText(filePath, "Dosya içeriği");
+            }
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            var selectedImage = listBox.SelectedItem as string;
+
+            if (selectedImage != null)
+            {
+
+
+                SliderWindow sliderWindow = new SliderWindow();
+                sliderWindow.Source= selectedImage;
+                sliderWindow.Items=Items;
+                var result= sliderWindow.ShowDialog();
+          
+                if (result == true)
+                {
+
+                }
+
+
+            }
+        }
+
+        private void Click_AddFile(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFile = openFileDialog.FileName;
+
+                Items.Add(selectedFile);
+            }
+        }
+    }
+}
---
+++ EyeGalery/View/SliderWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace EyeGalery.View
+{
+    /// <summary>
+    /// Interaction logic for SliderWindow.xaml
+    /// </summary>
+    public partial class SliderWindow : Window, INotifyPropertyChanged
+    {
+        string _source { get; set; }
+       public string Source {
+            get => _source
+                ;
+            set
+            {
+                _source= value;
+                NotifyPropertyChanged();
+        
+             }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<string> Items { get; set; } = new();
+        public SliderWindow()
+        {
+            InitializeComponent();
+            DataContext=this;
+        }
+
+     
+       
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private bool itemFound = false; 
+        private int currentIndex = 0;
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            string sourceString = Source.ToString();
+
+            for (int i = 0; i < Items.Count-1; i++)
+            {
+                if (Items[i] == sourceString)
+                {
+                    itemFound = true;
+                    currentIndex = i;
+                    break;
+                }
+            }
+
+            if (itemFound)
+            {
+                
+                
+                Source=Items[currentIndex + 1];
+           
+                itemFound = false; 
+            }
+            else
+            {
+             Next.Visibility = Visibility.Hidden;
+              
+            }
+        }
+
+        private void BackPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            string sourceString = Source.ToString();
+
+            for (int i = Items.Count-1; i >0; i--)
+            {
+                if (Items[i] == sourceString)
+                {
+                    itemFound = true;
+                    currentIndex = i;
+                    break;
+                }
+            }
+
+            if (itemFound)
+            {
+
+
+                Source=Items[currentIndex - 1];
+
+                itemFound = false;
+                Next.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Back.Visibility = Visibility.Hidden;
+
+            }
+        }
+
+        private DispatcherTimer timer;
+
+        private void Auto_Play_Click(object sender, RoutedEventArgs e)
+        {
+           
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); 
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+      
+            if (currentIndex < Items.Count - 1)
+            {
+                currentIndex++;
+                Source = Items[currentIndex];
+            }
+            else
+            {
+                
+                timer.Stop();
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+            }
+        }
+    }
+}
